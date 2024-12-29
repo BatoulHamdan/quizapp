4da518466c3351a55a87ec16757b9f492ee4af91
BatoulHamdan <batoul.hamdan19@gmail.com>
2024-12-30 01:46:36 +0200

    Cleaning model codes

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,14 +9,8 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    // Use HasFactory and Notifiable traits
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<string>
-     */
     protected $fillable = [
         'firstname',
         'lastname',
@@ -25,25 +19,15 @@ class User extends Authenticatable
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<string>
-     */
     protected $hidden = [
-        'password',         // Hiding the password field
-        'remember_token',   // Hiding the remember token for security
+        'password',         
+        'remember_token',   
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
-            'password' => 'hashed',              // Casting the password to hashed value
+            'password' => 'hashed',              
         ];
     }
 }
