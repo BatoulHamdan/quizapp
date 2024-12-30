05cbceba6750616ca9c6824674eb61f38c754cab
BatoulHamdan <batoul.hamdan19@gmail.com>
2024-12-30 02:45:45 +0200

    Removing navbar of solver

---
+++ resources/views/layouts/guest.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ $title ?? 'Quizziz' }}</title>
+    <link rel="icon" href="{{ asset('images/quizziz.jpg') }}" type="image/jpg">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <main>
+        @yield('content')
+    </main>
+</body>
+</html>
